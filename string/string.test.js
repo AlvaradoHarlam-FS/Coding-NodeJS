bac78bf1b6bc735eb6572a09aa4820eb5fb820aa
AlvaradoHarlam-FS <hjalvarado@student.fullsail.edu>
2022-10-02 09:42:06 -0400

    Added Math.sqrt and Math.max to the testing Jest

--- string/string.test.js
+++ string/string.test.js
@@ -1,4 +1,4 @@
-const  { add, mul, div, sub } = require("./string");
+const  { add, mul, div, sub, max, sqrt } = require("./string");
 
 test('2 + 3 = 5', () => {
    expect(add(2, 3)).toBe(5);
@@ -16,6 +16,12 @@ test('89 - 81 = 8', () => {
     expect(sub(89, 81)).toBe(8);
 });
 
+test('Math.max' , () => {
+    expect(max(10, 20)).toBe(20);
+});
 
+test('Math.sqrt' , () => {
+    expect(sqrt(20)).toBe(4.47213595499958);
+});
 
 
